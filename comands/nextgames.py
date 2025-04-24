293d717687f07a5cedf8ee6d737e3ef140320420
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-04-24 09:00:02 -0300

    Update nextgames.py

--- comands/nextgames.py
+++ comands/nextgames.py
@@ -1,1 +1,6 @@
+from utils.messages import ProxGames
 
+def setup_message_nextgames(bot):
+    @bot.message_handler(commands=['proximosjogos'])
+    def message_nexgames(message):
+        bot.reply_to(message, ProxGames)
