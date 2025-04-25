6287e3caae06eb3112574fdcc1acd5cce385710f
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-04-25 07:50:20 -0300

    Update get_notices.py

--- scrappy/get_notices.py
+++ scrappy/get_notices.py
@@ -4,4 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
+url = 'https://draft5.gg/equipe/330-FURIA'
 
+options.add_experimental_option("prefs", prefs)
+options.add_argument("--headless")
+driver = webdriver.Chrome(options= options, service= service)
+
+driver.get(url)
