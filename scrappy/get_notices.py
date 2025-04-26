a9925169bf8842fbb3f7eb0eb235f271375fdf8c
Suttiago <tiagobsversut@gmail.com>
2025-04-26 15:12:25 -0300

    scrappy

--- scrappy/get_notices.py
+++ scrappy/get_notices.py
@@ -1,13 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
 url = 'https://draft5.gg/equipe/330-FURIA'
-
+options = Options()
+service = Service()
+prefs = {}
 options.add_experimental_option("prefs", prefs)
 options.add_argument("--headless")
 driver = webdriver.Chrome(options= options, service= service)
 
 driver.get(url)
+
+time.sleep(3)
+
+html = driver.page_source
+
+soup = BeautifulSoup(html,'html.parser')
+
+noticias = soup.find_all('a', class_='NewsCardSmall__NewsCardSmallContainer-sc-1q3y6t7-0')
+
+base_url = "https://draft5.gg"
+for noticia in noticias:
+    titulo = noticia.find('p').text.strip()
+    link = base_url + noticia['href']
+    print("Ultimas notícias da fúria"+link)
